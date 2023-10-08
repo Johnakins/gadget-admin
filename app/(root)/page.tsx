a61f64c8adc1d35ecfec77ceb0531c4e813c42ab
Johnakins <johnolawole1@gmail.com>
2023-10-08 04:23:56 +0100

    using zustand to manage state in Store Modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,24 @@
 "use client"
 
-import { Modal } from "@/components/ui/modal";
-import { UserButton } from "@clerk/nextjs";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+import { useEffect } from "react";
 
 
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => { }} >
-        Children
-      </Modal>
+      Root Page
     </div>
   )
 }
